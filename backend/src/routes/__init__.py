dd977a1e20dcb69cf2416360d95846e7470c8755
Raz Olewsky <114615504+razol102@users.noreply.github.com>
2024-08-15 16:23:14 +0300

    faq_routes

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -4,6 +4,7 @@ from src.routes.other import other_routes
 from src.routes.user_routes import user_routes
 from src.routes.dog_routes import dog_routes
 from src.routes.collar_routes import collar_routes
+from src.routes.faq_routes import faq_routes
 
 
 def init_routes(app):
@@ -13,3 +14,4 @@ def init_routes(app):
     app.register_blueprint(other_routes)
     app.register_blueprint(fitness_routes)
     app.register_blueprint(dog_care_info_routes)
+    app.register_blueprint(faq_routes)
