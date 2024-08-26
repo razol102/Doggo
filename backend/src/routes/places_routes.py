42a301543620c57a733eac5932b57eecee9071d7
Niz123 <nizan.naor11@gmail.com>
2024-08-26 17:06:55 +0300

    Adding task.py (threads) and adding "add activity" endpoint

--- backend/src/routes/places_routes.py
+++ backend/src/routes/places_routes.py
@@ -42,33 +42,3 @@ def get_places_by_type():
 
     return jsonify(result), HTTP_200_OK
 
-
-@places_routes.route("/api/places/favorite_by_type", methods=['PUT'])
-def set_favorite_place_by_type():
-    data = request.json
-    required_data = {"place_name", "address", "type"}
-
-    reset_favorite_places_query = f"UPDATE {PLACES_TABLE} SET is_favorite = FALSE WHERE type = '{data.get('type')}'"
-    set_favorite_place_query = f"""
-    UPDATE {PLACES_TABLE}
-    SET is_favorite = TRUE
-    WHERE place_name = '{data.get('place_name')}' AND address = '{data.get('address')}' AND type = '{data.get('type')}';
-    """
-
-    try:
-        if not required_data.issubset(data.keys()):
-            missing_fields = required_data - data.keys()
-            raise MissingFieldsError(missing_fields)
-
-        db = load_database_config()
-
-        with psycopg2.connect(**db) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(reset_favorite_places_query, (data.get('type')))
-                cursor.execute(set_favorite_place_query, data)
-                connection.commit()
-
-    except(Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"error": str(error)}), HTTP_400_BAD_REQUEST
-
-    return "", HTTP_200_OK
